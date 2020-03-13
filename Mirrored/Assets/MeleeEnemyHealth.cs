460667cdd3b76bc2309b29c9cd72f6870a4bb7dc
MichaelPot <60535268+MichaelPot@users.noreply.github.com>
2020-03-12 22:00:03 -0400

    Shotgun

---
+++ Mirrored/Assets/MeleeEnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyHealth : MonoBehaviour {
+
+    int health = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int amount)
+    {
+        if (health > 0)
+        {
+            health -= amount;
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
